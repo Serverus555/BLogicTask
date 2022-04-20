8bd5439b2c11b6c6e999979a2bb0268bf4ff4484
Serverus <Serverus555@users.noreply.github.com>
2022-04-20 07:32:46 +0700

    Advanced search

--- src/main/java/me/serverus/blogictask/repository/AbstractDao.java
+++ src/main/java/me/serverus/blogictask/repository/AbstractDao.java
@@ -3,13 +3,15 @@ package me.serverus.blogictask.repository;
 import me.serverus.blogictask.repository.interfaces.IAbstractDao;
 import me.serverus.blogictask.utils.Filter;
 import me.serverus.blogictask.utils.Sort;
-import org.hibernate.query.criteria.internal.OrderImpl;
 
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.*;
+import javax.persistence.metamodel.Attribute;
+import javax.persistence.metamodel.EntityType;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 
 public abstract class AbstractDao<T> implements IAbstractDao<T> {
@@ -40,13 +42,25 @@ public abstract class AbstractDao<T> implements IAbstractDao<T> {
         Root<T> root = cq.from(typeClass);
         Predicate predicate = cb.conjunction();
         for (Filter filter : filters) {
-            cb.and(predicate, cb.equal(root.get(convertColumnName(filter.column)), filter.value));
+            Attribute.PersistentAttributeType persistenceType = root.getModel()
+                    .getDeclaredAttribute(filter.column).getPersistentAttributeType();
+
+            if (persistenceType == Attribute.PersistentAttributeType.BASIC) {
+                Path<String> path = root.get(filter.column);
+                Optional<Predicate> p = getPredicate(cb, path.getJavaType(), path ,filter.value);
+                if (p.isPresent()) {
+                    predicate = cb.and(p.get());
+                }
+            }
+            else {
+                predicate = cb.and(createFilterSubPredicate(filter, cb, root));
+            }
         }
 
         cq.where(predicate);
 
         Function<Expression<?>, Order> orderFunc = sort.isAscend ? cb::asc : cb::desc;
-        cq.orderBy(orderFunc.apply(root.get(convertColumnName(sort.column))));
+        cq.orderBy(orderFunc.apply(root.get(sort.column)));
 
         return em.createQuery(cq)
                 .setFirstResult(from)
@@ -71,10 +85,37 @@ public abstract class AbstractDao<T> implements IAbstractDao<T> {
 
     /**
      * Convert column name for work with jpa naming. Can change name to another like "someColumn" -> "some_column_id"
+     *
      * @param column camelCase
      * @return snake_case
      */
     protected String convertColumnName(String column) {
         return column.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+    }
+
+    private Predicate createFilterSubPredicate(Filter filter, CriteriaBuilder cb, Path<?> path) {
+        Class<?> nestedEntityClass = path.getJavaType();
+        EntityType<?> entityType = em.getMetamodel().entity(nestedEntityClass);
+        Predicate predicate = cb.disjunction();
+        Set<? extends Attribute<?, ?>> a = entityType.getDeclaredAttributes();
+        for (Attribute<?, ?> column : a) {
+            Optional<Predicate> p = getPredicate(cb, column.getJavaType(), path.get(column.getName()), filter.value);
+            if (p.isPresent()) {
+                predicate = cb.or(p.get());
+            }
+        }
+        return predicate;
+    }
+
+    protected Optional<Predicate> getPredicate(CriteriaBuilder cb, Class<?> columnClass, Path<String> path, String value) {
+        Predicate predicate = null;
+        if (columnClass == String.class) {
+            predicate = cb.like(path, "%"+value+"%");
+        }
+        else if (columnClass == Long.class && value.matches("[0-9]")) {
+            predicate = cb.equal(path, value);
+        }
+
+        return Optional.ofNullable(predicate);
     }
 }
--- src/main/java/me/serverus/blogictask/repository/AssignmentDao.java
+++ src/main/java/me/serverus/blogictask/repository/AssignmentDao.java
@@ -4,11 +4,32 @@ import me.serverus.blogictask.model.Assignment;
 import me.serverus.blogictask.repository.interfaces.IAssignmentDao;
 
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import java.util.Optional;
 
 @Stateless
 public class AssignmentDao extends AbstractDao<Assignment> implements IAssignmentDao {
 
     public AssignmentDao() {
         super(Assignment.class);
+    }
+
+    @Override
+    protected Optional<Predicate> getPredicate(CriteriaBuilder cb, Class<?> columnClass, Path<String> path, String value) {
+        Predicate predicate = null;
+        if (columnClass == Assignment.ControlStatus.class) {
+            predicate = cb.equal(path, Assignment.ControlStatus.valueOf(value));
+        }
+        if (columnClass == Assignment.ExecuteStatus.class) {
+            predicate = cb.equal(path, Assignment.ExecuteStatus.valueOf(value));
+        }
+
+
+        if (predicate == null) {
+            return super.getPredicate(cb, columnClass, path, value);
+        }
+        return Optional.of(predicate);
     }
 }
